c1196da3415f7fbf188cfdc6c6975e16c8df05cf
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-15 15:33:53 +0300

    throttle & vimeo npm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.getElementById('vimeo-player');
+console.log(iframe);
+
+let player = new Vimeo(iframe);
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+var throttle = require('lodash.throttle');
+
+console.log(throttle)
