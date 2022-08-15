88ad3e06ccec977cea4c9f44c1e0a005fce2c98b
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-15 17:47:23 +0300

    form manipulating

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,44 @@
 import throttle from "lodash.throttle";
 
+
+//VARIABLES
+let myForm = document.querySelector('.feedback-form');
+let formData = new FormData(myForm);
+let input = document.querySelector('input');
+let message = document.querySelector('[name="message"]');
+let userData = {};
+
+
+//LISTENERS
+window.addEventListener('load', parseDataOnLoad)
+
+myForm.addEventListener('input', throttle(gatherData , 500))
+
+myForm.addEventListener('submit', submitData)
+
+// CALLBACKS
+function gatherData (){
+    formData.set('email', `${input.value}`)
+    formData.set('message',`${message.value}`)
+   
+    userData.email = formData.get('email');
+    userData.message = formData.get('message');
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData))
+}
+
+function parseDataOnLoad(){
+    let savedSettings = localStorage.getItem("feedback-form-state");
+    if (savedSettings !== null){
+        parsedSettings = JSON.parse(savedSettings);
+        
+        input.value = parsedSettings.email;
+        message.value = parsedSettings.message;
+    }
+}
+
+function submitData (){
+    event.preventDefault();
+    myForm.reset();
+}
+
